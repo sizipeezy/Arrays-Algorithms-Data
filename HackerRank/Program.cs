ec3f4b8ec9d94faf27258abfc7c98b3b285dfa61
Sezen <sezen.ibrahim3@gmail.com>
2023-01-13 21:50:34 +0200

    DiagnoalDifference

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -1,41 +1,74 @@
-﻿internal class Program
+﻿using System.ComponentModel.Design.Serialization;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        var a = new int[] {5, 6, 7 };
-        var b = new int[] {3, 6, 10 };
+        int[,] arr ={{11, 2, 4},
+                     {4 , 5, 6},
+                     {10, 8, -12}};
 
-        var expected = CompareTriplets(a, b);
-
-        Console.WriteLine(string.Join(" ", expected.ToList()));
+        Console.WriteLine(DiagonalDifference(arr));
     }
-    public static List<int> CompareTriplets(int[] a, int[] b)
+
+    public static int DiagonalDifference(int[,] arr)
+    {
+        var d1 = 0;
+        var d2 = 0;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            d1 += arr[i, i];
+        }
+
+        int counter = arr.GetLength(1) - 1;
+
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            d2 += arr[i, counter];
+            counter--;
+        }
+
+        return Math.Abs(d1 - d2);
+    }
+    public static long aVeryBigSum(List<long> ar)
+    {
+        long sum = 0;
+        foreach (var item in ar)
+        {
+            sum += item;
+        }
+
+        return sum;
+    }
+
+    public static List<int> CompareTriplets(List<int> a, List<int> b)
     {
 
         var alicePoints = 0;
         var bobPoints = 0;
 
         var startIndex = 0;
-        var endIndex = a.Length - 1;
+        var endIndex = a.Count - 1;
 
         var result = new List<int>();
 
-            for (int i = startIndex; i <= endIndex; i++)
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            if (a[i] == b[i])
             {
-                if (a[i] == b[i])
-                {
-                    continue;
-                }
-
-                if (a[i] > b[i])
-                {
-                    alicePoints++;
-                }
-                else
-                {
-                    bobPoints++;
-                }
+                continue;
             }
+
+            if (a[i] > b[i])
+            {
+                alicePoints++;
+            }
+            else
+            {
+                bobPoints++;
+            }
+        }
 
         result.Add(alicePoints);
         result.Add(bobPoints);
